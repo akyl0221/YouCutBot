59ed1a36178aa5cb242d9c366255f172a7e55d64
KEL <akyl_0221@mail.ru>
2019-07-22 14:59:38 +0600

    refactor

--- Bot.py
+++ Bot.py
@@ -14,49 +14,85 @@ media_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
 
 
 def startCommand(bot, update):
-    text = 'Бот работает просто:\n' \
+    text = 'Привет, я YouCut бот который вырежет для тебя видео и сконвертирует его в аудио\n'\
+           'Бот работает просто:\n' \
            '1) Вставьте ссылку с YouTube видео и напишите промежуток\n ' \
            'который вам надо вырезать (формат 00:00-00:00 минуты секунды) \n' \
            '2) Подождите несколько минут \n' \
            'Пример: \n' \
-           'https://www.youtube.com/watch?v=UYwF-jdcVjY 02:10-03:05\n'
-    bot.send_message(chat_id=update.message.chat_id, text='Привет, я YouCut бот который вырежет для тебя видео\n'
-                     + text + 'Помощь: /help')
+           'https://www.youtube.com/watch?v=UYwF-jdcVjY 02:10-03:05\n' \
+           'Помощь: /help'
+    bot.send_message(chat_id=update.message.chat_id, text=text)
 
 
 def videoMessage(bot, update):
     text = update.message.text.split()
+    regex_url = re.compile(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$')
+    regex_timeline = re.compile(r'(([0-9][0-9]|[0-9]):[0-5][0-9])-(([0-9][0-9]|[0-9]):[0-5][0-9])')
     if (len(text) == 1):
-        bot.send_message(chat_id=update.message.chat_id, text="Wrong")
+        bot.send_message(chat_id=update.message.chat_id, text='Неправильный ввод. '
+                                                              'Пожалуйста введите ссылку '
+                                                              'Youtube и временной промежуток')
+
     else:
-        url = text[0]
-        regex = re.compile(r'^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$')
-        if (re.match(regex, url) is not None):
-            interval = text[1].split('-')
-            _from = interval[0].split(':')
-            _to = interval[1].split(':')
-            start_time = (int(_from[0]) * 60) + int(_from[1])
-            end_time = (int(_to[0]) * 60) + int(_to[1])
+        bot.send_message(chat_id=update.message.chat_id, text='Проверка введенных данных...')
+        url = ''
+        timeline = ''
+        for i in text:
+            if (re.match(regex_url, i) is not None):
+                url = i
+            if (re.match(regex_timeline, i) is not None):
+                timeline = i
 
-            ydl_opts = {
-                'format': 'best',
-                'outtmpl': 'media/%(title)s.%(ext)s'
-            }
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                result = ydl.extract_info(url, download=True)
+        if url != '':
+            if timeline != '':
+                interval = timeline.split('-')
+                _from = interval[0].split(':')
+                _to = interval[1].split(':')
+                start_time = (int(_from[0]) * 60) + int(_from[1])
+                end_time = (int(_to[0]) * 60) + int(_to[1])
 
-            name = result['title']
-            video_input = media_dir + '/' + name + '.mp4'
-            video_output = media_dir + '/' + name + "-Cut.mp4"
-            ffmpeg_extract_subclip(video_input, start_time, end_time, targetname=video_output)
-            video = open(video_output, 'rb')
-            bot.send_video(chat_id=update.message.chat_id, video=video, supports_streaming=True)
+                if end_time < start_time:
+                    current = start_time
+                    start_time = end_time
+                    end_time = current
+
+                ydl_opts = {
+                    'format': 'best',
+                    'outtmpl': 'media/%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192',
+                    }],
+                }
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    result = ydl.extract_info(url, download=False)
+                duration = result['duration']
+
+                if end_time > duration:
+                    bot.send_message(chat_id=update.message.chat_id, text='Выбранный промежуток длиннее чем видео')
+                else:
+                    bot.send_message(chat_id=update.message.chat_id, text='Скачивание видео...')
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([url])
+                    name = result['title']
+                    audio_input = media_dir + '/' + name + '.mp3'
+                    audio_output = media_dir + '/' + name + "-Cut.mp3"
+                    bot.send_message(chat_id=update.message.chat_id, text='Конвертирование и обрезка...')
+
+                    ffmpeg_extract_subclip(audio_input, start_time, end_time, targetname=audio_output)
+                    video = open(audio_output, 'rb')
+                    bot.send_message(chat_id=update.message.chat_id, text='Отправка аудио...')
+                    bot.send_audio(chat_id=update.message.chat_id, audio=video)
+            else:
+                bot.send_message(chat_id=update.message.chat_id, text='Пожалуйста введите промежуток вместе с адресом')
         else:
-            bot.send_message(chat_id=update.message.chat_id, text='Wrong url it is not Youtube')
+            bot.send_message(chat_id=update.message.chat_id, text='Неправильный адресс это не Youtube')
 
 
 def helpCommand(bot, update):
-    text = 'Бот работает просто:\n' \
+    text = 'Инструкция:\n' \
            '1) Вставьте ссылку с YouTube видео и напишите промежуток\n ' \
            'который вам надо вырезать (формат 00:00-00:00 минуты секунды) \n' \
            '2) Подождите несколько минут \n' \
