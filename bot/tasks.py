141ec30b22ef42ef076aa6abbed3818df2d6b9f8
kel <akyl_0221@mail.ru>
2019-07-29 21:15:52 +0600

    add celery not worked

---
+++ bot/tasks.py
@@ -0,0 +1,67 @@
+from celery import Celery
+from slugify import slugify
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import youtube_dl
+import os
+
+
+app = Celery('YouCutBot.tasks', broker='redis://localhost//',include=['bot.tasks'])
+
+media_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
+
+
+@app.task
+def download_convert(url, download, timeline=0):
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'media/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        if download:
+            ydl.download([url])
+        else:
+            result = ydl.extract_info(url, download=False)
+            duration = result['duration']
+            name = result['title'].replace('?', '').replace('"', '\'')
+            start_time, end_time = get_interval(timeline)
+            return start_time, end_time, duration, name
+
+
+@app.task
+def cut_audio(name, start_time, end_time):
+    files = os.listdir(media_dir)
+    for file in files:
+        for part in name.split():
+            print(part)
+            if part in name:
+                audio_input = media_dir + '/' + file
+                break
+    audio_output = media_dir + '/' + slugify(name) + '-Cut.mp3'
+
+    ffmpeg_extract_subclip(audio_input, start_time, end_time, targetname=audio_output)
+    audio = open(audio_output, 'rb')
+    os.remove(audio_input)
+    return audio, audio_output
+
+
+def get_interval(timeline):
+    interval = timeline.split('-')
+    _from = interval[0].split(':')
+    _to = interval[1].split(':')
+    if len(_from) == 3:
+        start_time = ((int(_from[0]) * 60) + int(_from[1])) * 60 + int(_from[2])
+        end_time = ((int(_to[0]) * 60) + int(_to[1])) * 60 + int(_to[2])
+    else:
+        start_time = (int(_from[0]) * 60) + int(_from[1])
+        end_time = (int(_to[0]) * 60) + int(_to[1])
+
+    if end_time < start_time:
+        current = start_time
+        start_time = end_time
+        end_time = current
+    return start_time, end_time
